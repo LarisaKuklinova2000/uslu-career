f4aa1a0aa99ddc80d262e6688add79b12853f460
IvanLarionov90 <gnosticism100500@gmail.com>
2023-02-09 10:11:14 +0500

    add cards styles, add fetch cards

--- src/js/index.js
+++ src/js/index.js
@@ -22,132 +22,35 @@ document.addEventListener('DOMContentLoaded', function () {
             this.feed = feed;
         }
 
-        closeAllModal() {
-            document.querySelectorAll('.modal__wrapper').forEach(item => {
-                item.style.left = '-100%';
-            });
-        }
-
         renderCards() {
             const element = document.createElement('div');
-            element.classList.add('card', 'cat-card');
-            element.style.background = `url(${this.img_link}) center center/cover no-repeat`;
+            element.classList.add('card');
+            element.style.background = `white`;
             element.innerHTML = `
-                <div class="title-content">
-                    <h3>${this.name}</h3>
-                    <hr/>
-                    <div class="intro">хороший котя</div>
+                <div class="card__title">${this.employer}</div>
+                <div class="card__employer">${this.jobTitle}</div>
+                <div class="card__salary">${this.salary} руб.</div>
+                <div class="card__tags">
+                    <div class="card__tags-item">#Cтудент старших курсов</div>
+                    <div class="card__tags-item">#Полная занятость</div>
+                    <div class="card__tags-item">#Опыт не требуется</div>
                 </div>
-                <div class="card-info">${this.description}</div>
-                <div class="gradient-overlay"></div>
-                <div class="color-overlay"></div>
-                <i class="fa-solid fa-arrow-up-right-from-square" id="openModal"></i>
-                <div class="edit-or-delete">
-                    <i class="fa-solid fa-trash fa-trash-card" data-id=${this.id}></i>
-                    <i class="fa-solid fa-pen-to-square" data-id=${this.id}></i>
+                <div class="card__buttons">
+                    <button class="moreInfoBtn">Подробнее</button>
+                    <button class="feed" data-id=${this.id}>Связаться</button>
                 </div>
             `;
-            document.querySelector('.cards__wrapper').append(element);
-
-        }
-
-        renderModal() {
-            const element = document.createElement('div');
-            element.classList.add('modal__window');
-            element.innerHTML = `
-                <div class="modal__wrapper">
-                    <i class="fa-solid fa-trash fa-trash-modal" data-id=${this.id}></i>
-                    <i class="fa-solid fa-pen-to-square" data-id=${this.id}></i>
-                    <i class="fa-regular fa-circle-xmark" id="modal__close"></i>
-                    <div class="modal__photo">
-                        <img class="modalCatImg" src="${this.img_link}" alt="catimg">
-                    </div>
-                    <div class="modal__info">
-                        <h3 class="modal__name">${this.name}</h3>
-                        <div class="modal__intro">хороший котя</div>
-                        <div class="modal__descr">${this.description}</div>
-                        <div class="utility-info">
-                            <ul class="utility-list">
-                                <li class="raiting">${'<i class="fa-solid fa-star"></i>'.repeat(this.rate) + '<i class="fa-regular fa-star"></i>'.repeat(10-this.rate)}</li>
-                                <li class="birth-date">${this.age}</li>
-                                <li class="favourite">${this.favor()}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            `;
-            document.querySelector('.main-overlay').append(element);
-            document.querySelectorAll('.modal__wrapper').forEach(item => {
-                item.style.left = '-50%';
-            });
-        }
-
-        setListeners() {
-
-            const cards = document.querySelectorAll('.card'),
-                  overlay = document.querySelector('.main-overlay');
-
-            cards.forEach((card, i) => {
-                card.addEventListener('click', (e) => {
-                    if (e.target.classList.contains('likes')) {
-                        e.target.classList.toggle('fa-solid');
-                        e.target.classList.toggle('fa-regular');
-                    }
-                    if (e.target.id == 'openModal') {
-                        document.querySelector('.main-overlay').style.visibility = 'visible';
-                        document.querySelectorAll('.modal__wrapper')[i].style.left = '50%';
-                        document.body.style.overflow = 'hidden';
-                    }
-                });
-            });
-
-            overlay.addEventListener('click', (e) => {
-                if (e.target.id == 'modal__close') {
-                    document.querySelector('.main-overlay').style.visibility = 'hidden';
-                    document.body.style.overflow = '';
-                    this.closeAllModal();
-                    document.querySelector('.form').style.left = '-50%';
-                }
-            });
-
-            document.querySelector('#openForm').addEventListener('click', () => {
-                document.querySelector('.form').style.left = '50%';
-                document.querySelector('.main-overlay').style.visibility = 'visible';
-                document.body.style.overflow = 'hidden';
-            });
-
-            document.querySelectorAll('.fa-pen-to-square').forEach((item) => {
-                item.addEventListener('click', this.rewriteCard);
-            });
-
-            document.querySelectorAll('.fa-trash-card').forEach((item, i) => {
-                item.addEventListener('click', this.delCard);
-                item.addEventListener('click', () => {
-                    const arrCards = document.querySelectorAll('.card');
-                    arrCards[i].style.display = 'none';
-                });
-            });
-
-            document.querySelectorAll('.fa-trash-modal').forEach((item, i) => {
-                item.addEventListener('click', this.delCard);
-                item.addEventListener('click', () => {
-                    const arrCards = document.querySelectorAll('.card');
-                    arrCards[i].style.display = 'none';
-                    this.closeAllModal();
-                    document.querySelector('.main-overlay').style.visibility = 'hidden';
-                    document.body.style.overflow = '';
-                });
-            });
+            document.querySelector(`#${this.category}Cards`).append(element);
         }
 
         init() {
             this.renderCards();
-            this.renderModal();
-            this.setListeners();
         }
     }
 
     getResource('https://raw.githubusercontent.com/LarisaKuklinova2000/uslu-career/main/vacancy.json')
-        .then(res => console.log(res.vacancy));
+        .then(res => res.vacancy.map(({id, category, employer, jobTitle, salary, link, feed}) => {
+            new EmployerCard(id, category, employer, jobTitle, salary, link, feed).init()
+        }));
 
 });
