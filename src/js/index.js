97109cd5ad48d5bfc81077f11a7592c76ca27df1
IvanLarionov90 <gnosticism100500@gmail.com>
2023-02-09 11:36:56 +0500

    add smooth scroll to links, add scroll to top

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,59 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    document.querySelectorAll('a[href^="#"').forEach(link => {
+
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+    
+            let href = this.getAttribute('href').substring(1);
+    
+            const scrollTarget = document.getElementById(href);
+    
+            const topOffset = document.querySelector('.scrollto').offsetHeight;
+            // const topOffset = 0; // если не нужен отступ сверху 
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - topOffset;
+    
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    const btnUp = {
+        el: document.querySelector('.btn-up'),
+        show() {
+          // удалим у кнопки класс btn-up_hide
+          this.el.classList.remove('btn-up_hide');
+        },
+        hide() {
+          // добавим к кнопке класс btn-up_hide
+          this.el.classList.add('btn-up_hide');
+        },
+        addEventListener() {
+          // при прокрутке содержимого страницы
+          window.addEventListener('scroll', () => {
+            // определяем величину прокрутки
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+            scrollY > 400 ? this.show() : this.hide();
+          });
+          // при нажатии на кнопку .btn-up
+          document.querySelector('.btn-up').onclick = () => {
+            // переместим в начало страницы
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: 'smooth'
+            });
+          }
+        }
+      }
+      
+      btnUp.addEventListener();
+
     const getResource = async (url) => {
         const result = await fetch(url);
         if (!result.ok) {
@@ -30,11 +83,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="card__title">${this.employer}</div>
                 <div class="card__employer">${this.jobTitle}</div>
                 <div class="card__salary">${this.salary} руб.</div>
-                <div class="card__tags">
-                    <div class="card__tags-item">#Cтудент старших курсов</div>
-                    <div class="card__tags-item">#Полная занятость</div>
-                    <div class="card__tags-item">#Опыт не требуется</div>
-                </div>
+                <div class="card__tags">${this.tags.join('<br>')}</div>
                 <div class="card__buttons">
                     <button class="moreInfoBtn">Подробнее</button>
                     <button class="feed" data-id=${this.id}>Связаться</button>
@@ -49,8 +98,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     getResource('https://raw.githubusercontent.com/LarisaKuklinova2000/uslu-career/main/vacancy.json')
-        .then(res => res.vacancy.map(({id, category, employer, jobTitle, salary, link, feed}) => {
-            new EmployerCard(id, category, employer, jobTitle, salary, link, feed).init()
+        .then(res => res.vacancy.map(({id, category, employer, jobTitle, salary, tags, link, feed}) => {
+            new EmployerCard(id, category, employer, jobTitle, salary, tags, link, feed).init()
         }));
 
 });
